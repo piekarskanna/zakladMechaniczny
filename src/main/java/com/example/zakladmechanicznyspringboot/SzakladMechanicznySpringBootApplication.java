5eb4ff598afac9fe1da92fc227cc1fc8b21d28d3
Michal Zabinski <zabinski@student.agh.edu.pl>
2022-11-14 16:20:48 +0100

    zarys projektu zakladu

---
+++ src/main/java/com/example/zakladmechanicznyspringboot/SzakladMechanicznySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.zakladmechanicznyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzakladMechanicznySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SzakladMechanicznySpringBootApplication.class, args);
+    }
+
+}
