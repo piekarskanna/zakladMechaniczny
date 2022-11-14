5bc641839af0dbf31a7d2443fe3d3c5dc5b19364
Michal Zabinski <zabinski@student.agh.edu.pl>
2022-11-14 16:34:14 +0100

    poprawa

--- src/test/java/com/example/zakladmechanicznyspringboot/zakladMechanicznySpringBootApplicationTests.java
+++ src/test/java/com/example/zakladmechanicznyspringboot/zakladMechanicznySpringBootApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SzakladMechanicznySpringBootApplicationTests {
+class zakladMechanicznySpringBootApplicationTests {
 
     @Test
     void contextLoads() {
